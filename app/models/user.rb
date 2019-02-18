3438790c8c1a266918656cc5beb6f7f9b866e2d3
yoann roncajolo <yoann.roncajolo@gmail.com>
2019-02-18 18:25:39 +0100

    added foreign keys to bookings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :bookings
   has_many :animations
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
