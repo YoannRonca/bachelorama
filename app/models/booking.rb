0edd9a161cf09cd50623b6d0cc371c1566b81992
yoann roncajolo <yoann.roncajolo@gmail.com>
2019-02-19 11:04:10 +0100

    seedbookingadded

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :animation
   belongs_to :user
 
-  validates :arrival_time, presence: true, uniqueness: true
-  validates :departure_time, presence: true, uniqueness: true
   validates :place, presence: true
 end
