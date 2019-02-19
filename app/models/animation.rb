643aeac766e6ce9b6ccb4deb6623761b32f548c2
yoann roncajolo <yoann.roncajolo@gmail.com>
2019-02-19 16:24:08 +0100

    upload photo and card

--- app/models/animation.rb
+++ app/models/animation.rb
@@ -3,6 +3,8 @@ class Animation < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
 
+  mount_uploader :photo, PhotoUploader
+
   validates :title, presence: true
   validates :category, presence: true
   validates :price, presence: true, numericality: { only_interger: true }
