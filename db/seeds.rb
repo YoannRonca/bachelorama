7d0654aed7d2cb4151f9c15396306ea5a6b1339a
Bruno Flores <brunogfloresvela@gmail.com>
2019-02-19 09:06:18 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Deleting DB....."
+
+User.destroy_all
+Animation.destroy_all
+
+
+
+puts 'Creating 6 animations...'
+
+Animation.create(title: "Clown yoself!", category: "clown", price:"200")
+Animation.create(title: "Throw yo'dwarf", category: "dwarf", price:"500")
+Animation.create(title: "Better than Ariel", category: "mermaid", price:"100")
+Animation.create(title: "Lamas in Pajamas", category: "lama", price:"800")
+Animation.create(title: "Gorillaz like in N", category: "monkey", price:"400")
+Animation.create(title: "Lose yo wife", category: "stripper", price:"1000")
+
+User.create(email: "yoann@gmail.com", password: "12345")
+User.create(email: "livia@gmail.com", password: "5678")
+User.create(email: "muriel@gmail.com", password: "98765")
+User.create(email: "bruno@gmail.com", password: "54321")
+
+puts 'Finished!'
