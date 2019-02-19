871f5ad3ba0a6eb4caa65475dcdca1811196adc8
Livia Lortscher <livialortscher@hotmail.com>
2019-02-19 17:22:42 +0100

    review done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,14 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    authorize @review
     @animation = Animation.find(params[:animation_id])
   end
 
   def create
     @animation = Animation.find(params[:animation_id])
     @review = Review.new(review_params)
+    authorize @review
     @review.animation = @animation
     @review.user = current_user
     if @review.save
