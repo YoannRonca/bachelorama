5e783603169353f7cbb22e2ea161cef9f177194c
Livia Lortscher <livialortscher@hotmail.com>
2019-02-20 14:30:42 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_02_20_115319) do
 
   # These are extensions that must be enabled in order to support this database
@@ -24,10 +23,10 @@ ActiveRecord::Schema.define(version: 2019_02_20_115319) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
-    t.float "latitude"
-    t.float "longitude"
     t.string "address"
     t.string "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_animations_on_user_id"
   end
 
