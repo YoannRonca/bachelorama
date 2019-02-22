5077c10d34b66429c9ac1a05aa7b4f8f0f0c2ba3
yoann roncajolo <yoann.roncajolo@gmail.com>
2019-02-22 15:09:34 +0100

    improvement2

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,6 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css"  // Note this is important!
 
+flatpickr(".datepickershow", {inline: true})
+
 flatpickr(".datepicker", {})
